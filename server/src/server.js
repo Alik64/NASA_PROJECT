280e0a19acb28060f048e01c3d9406668c064925
Albert Barsamov <abarsamov@worldia.com>
2023-11-27 23:05:11 +0100

    feat: update planets model + implement start server async func

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 const app = require("./app");
+const { loadingPlanetsData } = require("./models/planets.model");
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(PORT);
-});
+async function startServer() {
+  await loadingPlanetsData();
+  server.listen(PORT, () => {
+    console.log(PORT);
+  });
+}
+
+startServer();
