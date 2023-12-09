87de250f02a904439dc71db98e52e08fc63d977f
Albert Barsamov <abarsamov@worldia.com>
2023-12-09 11:36:21 +0100

    implement serving frontend build from express server

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,22 @@
+const path = require("path");
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const cors = require("cors");
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (res, req) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
