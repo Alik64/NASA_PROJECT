b451a55865c97284cdb56e9b456d068e7c2def53
Albert Barsamov <abarsamov@worldia.com>
2023-12-19 14:53:37 +0100

    chore: implement addNewLaunch

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,12 +1,12 @@
 const launches = new Map();
-
+let latestFlightNumber = 100;
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration 2000",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27,2030"),
   destination: "Kepler-442 b",
-  customer: ["AB64", "NASA", "Space X"],
+  customers: ["AB64", "NASA", "Space X"],
   upcoming: true,
   success: true,
 };
@@ -15,6 +15,18 @@ launches.set(launch.flightNumber, launch);
 function getAllLAunches() {
   return Array.from(launches.values());
 }
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(latestFlightNumber, {
+    ...launch,
+    success: true,
+    upcoming: true,
+    customers: ["AB64", "NASA", "Space X"],
+    flightNumber: latestFlightNumber,
+  });
+}
 module.exports = {
   getAllLAunches,
+  addNewLaunch,
 };
