a2c2c5c1168c40a4d1203a7bd71f48a57a7b5639
Albert Barsamov <abarsamov@worldia.com>
2024-01-05 09:59:56 +0100

    fix useLaunches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,7 +5,7 @@ const launch = {
   mission: "Kepler Exploration 2000",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27,2030"),
-  destination: "Kepler-442 b",
+  target: "Kepler-442 b",
   customers: ["AB64", "NASA", "Space X"],
   upcoming: true,
   success: true,
