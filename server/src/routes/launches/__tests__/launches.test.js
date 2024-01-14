ff24fbd48d39e617e737d8e80971ea81631b45b6
Albert Barsamov <abarsamov@worldia.com>
2024-01-14 23:27:42 +0100

    using super test for GET test

--- server/src/routes/launches/__tests__/launches.test.js
+++ server/src/routes/launches/__tests__/launches.test.js
@@ -1,11 +1,16 @@
+const request = require("supertest");
+const app = require("../../../app");
+
 describe("Test GET /launches", () => {
-  it("should respond with 200 success", () => {
-    const response = 200;
-    expect(response).toBe(200);
+  test("it should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect(200)
+      .expect("Content-type", /json/);
   });
 });
 describe("Test POST /launches", () => {
-  it("should respond with 200 success", () => {});
-  it("should catch missing required properties", () => {});
-  it("should catch invalid dates", () => {});
+  test("it should respond with 200 success", () => {});
+  test("it should catch missing required properties", () => {});
+  test("it should catch invalid dates", () => {});
 });
