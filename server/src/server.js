894345d166e035e53e785105bd444a430060f383
Albert Barsamov <albert.barsamov@gmail.com>
2024-04-06 20:00:17 +0200

    feat: connect server to Mongo db using mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,28 @@
+require('dotenv').config();
+
 const http = require("http");
+const mongoose = require("mongoose");
+
 const app = require("./app");
+
 const { loadingPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = process.env.MONGO_URL;
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
+
   await loadingPlanetsData();
   server.listen(PORT, () => {
     console.log(PORT);
