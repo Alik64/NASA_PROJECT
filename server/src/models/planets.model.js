22bc9a9e2b78f7c5adf70eecd3c05fa029b5fcd3
Albert Barsamov <albert.barsamov@gmail.com>
2024-04-09 14:43:34 +0200

    feat: exclude id + v mongodb fields

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -3,7 +3,6 @@ const path = require("path");
 const { parse } = require("csv-parse");
 const planets = require("./planets.mongo");
 
-
 function isHabitablePlanet(planet) {
   return (
     planet["koi_disposition"] === "CONFIRMED" &&
@@ -41,7 +40,7 @@ function loadingPlanetsData() {
   });
 }
 async function getAllPlanets() {
-  return await planets.find({});
+  return await planets.find({}, { _id: 0, __v: 0 });
 }
 async function savePlanet(planet) {
   try {
@@ -57,7 +56,7 @@ async function savePlanet(planet) {
       }
     );
   } catch (err) {
-    console.error(`Could not save planet ${err}`);
+    console.error(`Could not save planet: ${err}`);
   }
 }
 module.exports = {
